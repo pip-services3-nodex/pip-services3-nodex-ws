88b12dcfbe0ec14c6720e5f97ce214f7a47072de
Sergey Seroukhov <seroukhov@gmail.com>
2021-05-28 15:38:41 -0700

    Added new components

--- checkout.ps1
+++ checkout.ps1
@@ -8,6 +8,7 @@ git clone https://github.com/pip-services3-nodex/pip-services3-components-nodex.
 git clone https://github.com/pip-services3-nodex/pip-services3-container-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-data-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-rpc-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-grpc-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-messaging-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-swagger-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-mongodb-nodex.git
@@ -22,3 +23,10 @@ git clone https://github.com/pip-services3-nodex/pip-services3-nats-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-azure-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-aws-nodex.git
 git clone https://github.com/pip-services3-nodex/pip-services3-google-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-elasticsearch-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-prometheus-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-datadog-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-cassandra-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-fluentd-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-memcached-nodex.git
+git clone https://github.com/pip-services3-nodex/pip-services3-redis-nodex.git
